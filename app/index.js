53fce82b3ca087cd3a4e7385a015d21f81570172
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-07-15 13:08:22 -0300

    add component name user

--- app/index.js
+++ app/index.js
@@ -8,9 +8,19 @@ var HelloWorld = React.createClass({
     )
   }
 });
-
-
 ReactDOM.render(
   <HelloWorld />,
   document.getElementById('app')
 )
+
+var HelloUser = React.createClass({
+  render: function(){
+    return (
+      <div> Hello, {this.props.name}</div>
+    )
+  }
+});
+ReactDOM.render(
+  <HelloUser name="Marcelo"/>, 
+  document.getElementById('appUser')
+);
