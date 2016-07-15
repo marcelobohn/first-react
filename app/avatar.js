257391d2b9f5c09854bf3f563aa0732e05f0f00d
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-07-15 16:35:37 -0300

    change avatar

--- app/avatar.js
+++ app/avatar.js
@@ -1,27 +1,33 @@
+var USER_DATA = {
+  name: 'Marcelo Bohn',
+  username: 'marcelobohn',
+  image: '697053728180985856/Ox4uXGHh_400x400.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ProfilePic = function (props) {
-  return <img src={'https://photo.fb.com/' + props.username} />
+  return <img src={'https://pbs.twimg.com/profile_images/' + props.image} />
 }
 var ProfileLink = function (props) {
   return (
-    <a href={'https://www.fb.com/' + props.username}>
-      {props.username}
+    <a href={'https://twitter.com/' + props.user.username}>
+      {props.user.name}
     </a>
   )
 }
 var Avatar = function (props) {
   return (
     <div>
-      <ProfilePic username={props.username} />
-      <ProfileLink username={props.username} />
+      <ProfilePic image={props.user.image} />
+      <ProfileLink user={props.user} />
     </div>
   )
 }
 
 
 ReactDOM.render(
-  <Avatar username="marcelobohn" />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('appAvatar')
 );
