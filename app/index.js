506421e550b2e9ea5805b6310a85f072e98111ee
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-07-15 13:18:44 -0300

    add list friends

--- app/index.js
+++ app/index.js
@@ -21,6 +21,38 @@ var HelloUser = React.createClass({
   }
 });
 ReactDOM.render(
-  <HelloUser name="Marcelo"/>, 
+  <HelloUser name="Marcelo"/>,
   document.getElementById('appUser')
+);
+
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+});
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+ReactDOM.render(
+  <FriendsContainer />,
+  document.getElementById('appListUser')
 );
