291af0db1e281dfaeddf744814d6189d37cce054
Marcelo Bohn <marcelo.bohn@gmail.com>
2016-07-16 13:37:14 -0300

    use react-router

--- app/avatar.js
+++ app/avatar.js
@@ -47,3 +47,60 @@ ReactDOM.render(
   <Avatar user={USER_DATA} />,
   document.getElementById('appAvatar')
 );
+
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello React Program </div>
+    )
+  }
+});
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('appHello')
+)
+
+var HelloUser = React.createClass({
+  render: function(){
+    return (
+      <div> Hello, {this.props.name}</div>
+    )
+  }
+});
+ReactDOM.render(
+  <HelloUser name="Marcelo"/>,
+  document.getElementById('appUser')
+);
+
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+});
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+ReactDOM.render(
+  <FriendsContainer />,
+  document.getElementById('appListUser')
+);
--- app/index.js
+++ app/index.js
@@ -1,58 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var HelloWorld = React.createClass({
-  render: function () {
-    return (
-      <div> Hello React Program </div>
-    )
-  }
-});
 ReactDOM.render(
-  <HelloWorld />,
+  routes,
   document.getElementById('app')
 )
-
-var HelloUser = React.createClass({
-  render: function(){
-    return (
-      <div> Hello, {this.props.name}</div>
-    )
-  }
-});
-ReactDOM.render(
-  <HelloUser name="Marcelo"/>,
-  document.getElementById('appUser')
-);
-
-var FriendsContainer = React.createClass({
-  render: function(){
-    var name = 'Tyler McGinnis'
-    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
-    return (
-      <div>
-        <h3> Name: {name} </h3>
-        <ShowList names={friends} />
-      </div>
-    )
-  }
-});
-var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend){
-      return <li> {friend} </li>;
-    });
-    return (
-      <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
-      </div>
-    )
-  }
-});
-ReactDOM.render(
-  <FriendsContainer />,
-  document.getElementById('appListUser')
-);
